3957cb9bf22ea3177618deb4e36a7271dff0d3e9
AirenMarie <astringer92@gmail.com>
2024-08-02 21:15:12 -0400

    add methods in ShoppingCart class for calulation and resetting, create event listener for clearCartBtn

--- shopping-cart.js
+++ shopping-cart.js
@@ -126,6 +126,43 @@ class ShoppingCart {
             <p>${price}</p>
         </div>`);
   }
+  getCounts() {
+    return this.items.length;
+  }
+
+  calculateTaxes(amount) {
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+  }
+
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
+  }
+
+  clearCart() {
+    if (!this.items.length) {
+      alert("Your shopping cart is already empty");
+      return;
+    }
+
+    const isCartCleared = confirm(
+      "Are you sure you want to clear all the items from your shopping cart?"
+    );
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+      productsContainer.innerHTML = "";
+      totalNumberOfItems.textContent = 0;
+      cartSubTotal.textContent = 0;
+      cartTaxes.textContent = 0;
+      cartTotal.textContent = 0;
+    }
+  }
 }
 
 const cart = new ShoppingCart();
@@ -134,11 +171,15 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
 [...addToCartBtns].forEach((btn) => {
   btn.addEventListener("click", (event) => {
     cart.addItem(Number(event.target.id), products);
+    totalNumberOfItems.textContent = cart.getCounts();
+    cart.calculateTotal();
   });
 });
 
 cartBtn.addEventListener("click", () => {
-  const isCartShowing = !isCartShowing;
+  isCartShowing = !isCartShowing;
   showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
   cartContainer.style.display = isCartShowing ? "block" : "none";
 });
+
+clearCartBtn.addEventListener("click", cart.clearCart.bind(cart));
